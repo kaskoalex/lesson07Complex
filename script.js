38a3deff6a533e9eeb586032d3cb8c34675b2978
Alex <webmaster@papagai.de>
2022-02-05 18:04:39 +0100

    create

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 "use strict";
 
+const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let date = new Date();
+let day=date.getDay()
 
 
+for (let i = 0, len = week.length; i < len; i++) {
+  let html = week[i];
+  if (i > 4) html = html.italics(); // выходные
+  if ((i+1) == day) html = html.bold(); // сегодня
+
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  document.body.appendChild(div);
+}
