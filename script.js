51eed0a1768ba712c7358f39cbf447a66f9316ce
Alex <webmaster@papagai.de>
2022-02-06 17:53:29 +0100

    fix

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 "use strict";
 
-const week = ['Воскресенье','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 let date = new Date();
-let day=date.getDay()
+let day = date.getDay()
 
 
 for (let i = 0, len = week.length; i < len; i++) {
   let html = week[i];
-  if (i ==0 || i==6) html = html.italics(); // выходные
+  if (i == 0 || i == 6) html = html.italics(); // выходные
   if ((i) == day) html = html.bold(); // сегодня
 
   const div = document.createElement('div');
